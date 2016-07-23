d136395a462dd08e1cbb344a28116292f8784423
Sarthak Langde <sarthak.langde@gmail.com>
2016-07-23 18:04:23 +0530

    renamed projectform.html to projectform.php

--- PM/insert_activities.php
+++ PM/insert_activities.php
@@ -23,3 +23,36 @@
 	}
 ?>
 </select>
+<input name="activityname1" placeholder="Enter Activity Name">
+
+<button name="button1">Add Projects</button>
+
+<br>
+
+
+<select>
+<?php
+	$sql_get_paid='select * from priority_areas';
+	$result=mysql_query($sql_get_paid);
+	while ($row=mysql_fetch_array($result))
+	{
+		echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+		
+	}
+?>
+</select>
+
+<select>
+<?php
+	$sql_get_sid='select * from strategy_category';
+	$result=mysql_query($sql_get_sid);
+	while ($row=mysql_fetch_array($result))
+	{
+		echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+		
+	}
+?>
+</select>
+
+<input name="activityname2" placeholder="Enter Activity Name">
+<button name="button1">Add Projects</button>
