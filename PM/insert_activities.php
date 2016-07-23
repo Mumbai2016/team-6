9c907f637733d58fed68e3dfe8d1e964817dde2b
Sarthak Langde <sarthak.langde@gmail.com>
2016-07-23 17:19:18 +0530

    populate insert_activities.php with dropdowns of paid and sid from database

--- PM/insert_activities.php
+++ PM/insert_activities.php
@@ -1,3 +1,25 @@
+<?php include '..\connect.php';?>
 <h1> PM INSERT FORM </h1>
 <select>
-	
+<?php
+	$sql_get_paid='select * from priority_areas';
+	$result=mysql_query($sql_get_paid);
+	while ($row=mysql_fetch_array($result))
+	{
+		echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+		
+	}
+?>
+</select>
+
+<select>
+<?php
+	$sql_get_sid='select * from strategy_category';
+	$result=mysql_query($sql_get_sid);
+	while ($row=mysql_fetch_array($result))
+	{
+		echo '<option value="'.$row[0].'">'.$row[1].'</option>';
+		
+	}
+?>
+</select>
